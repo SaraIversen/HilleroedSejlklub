5e86c58adb767cddabe1ab732f4c064c6f8034b6
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2024-12-03 13:32:16 +0100

    changes

---
+++ SejlklubLibrary/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.SejlklubLibrary.Interfaces;
+
+namespace SejlklubLibrary.Models
+{
+    public class Member : IMember
+    {
+    }
+}
