b781acb1ce977076ebe19c8234635ed280ad36d6
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2024-12-04 09:51:26 +0100

    change to member, using library interface

--- SejlklubLibrary/Models/Member.cs
+++ SejlklubLibrary/Models/Member.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.SejlklubLibrary.Interfaces;
 
 namespace SejlklubLibrary.Models
 {
