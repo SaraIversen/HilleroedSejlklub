f6f137b27c4370d3e1bfe27f497473efc185a352
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2024-12-10 13:16:23 +0100

    ....

---
+++ SejlklubLibrary/Models/EventType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SejlklubLibrary.Models
+{
+    public enum EventType
+    {
+        Udflugt,Standerhejsning,Sejltur,Spisning
+    }
+}
