d5927eea9022b3cc55f401828c23e8f38579b8ff
Magnus Sverdrup <magger254@gmail.com>
2024-12-11 12:24:26 +0400

    Test of member class in program

--- SejlklubLibrary/Program.cs
+++ SejlklubLibrary/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SejlklubLibrary.Models;
 using SejlklubLibrary.Services;
+using System.Diagnostics.CodeAnalysis;
 
 //Test af boat class
 //_____Opretelse af objekt
@@ -36,3 +37,31 @@ foreach (Event item in test)
 { Console.WriteLine(item.ToString()); }
 
 Console.Write("_____________________________________________________________\n");
+
+//___Test af member class
+//___Oprettelse af member objekt
+Member m1 = new Member("Jens", "jens@mail.dk", "Vej 123", "84239212", MemberType.Senior);
+Console.WriteLine(m1);
+Console.Write("_____________________________________________________________\n");
+//___Oprettelse af member repository
+MemberRepository mRepo = new MemberRepository();
+//___Tilføjelse af member til repository
+mRepo.AddMember(m1);
+List<Member> members = mRepo.GetAll();
+
+foreach (Member m in members)
+{
+    Console.WriteLine(m);
+    Console.WriteLine();
+}
+
+Console.Write("_____________________________________________________________\n");
+//___Fjern et medlem fra repository
+mRepo.RemoveMember("12121212");
+members = mRepo.GetAll();
+
+foreach (Member m in members)
+{
+    Console.WriteLine(m);
+    Console.WriteLine();
+}
