484c64580348125ab1b7983174ca106e0106e103
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2024-12-17 14:09:54 +0100

    Filther i ShowEvents er funktionel

--- Razor/Pages/Events/UpdateEvents.cshtml.cs
+++ Razor/Pages/Events/UpdateEvents.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SejlklubLibrary.Interfaces;
 using SejlklubLibrary.Models;
 
@@ -13,16 +14,30 @@ namespace Razor.Pages.Events
         [BindProperty]
         public Event Event { get; set; }
 
+        [BindProperty]
+        public EventType ChosenEventType { get; set; }
+
+        public List<SelectListItem> SelectListEventTypes { get; set; }
+
         public UpdateEventsModel(IEventRepository eventRepository)
         {
             _eventRepository = eventRepository;
-            
+            createSelectListEventTypes();
         }
         public void OnGet(int editId)
         {
             Event=_eventRepository.GetEventByID(editId);
         }
-
+        public void createSelectListEventTypes()
+        {
+            SelectListEventTypes = new List<SelectListItem>();
+            SelectListEventTypes.Add(new SelectListItem("Vælg en Type", "-1"));
+            foreach (Event events in _eventRepository.GetAll())
+            {
+                SelectListItem selectListItem = new SelectListItem(events.EventType.ToString(), events.Id.ToString());
+                SelectListEventTypes.Add(selectListItem);
+            }
+        }
         public IActionResult OnPost() 
         {
             _eventRepository.UpdateEvent(Event);
