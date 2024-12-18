39f64ade6094834486136365ad592cfac4409283
Magnus Sverdrup <magger254@gmail.com>
2024-12-18 13:42:37 +0400

    Changes to IMemberRepository

--- SejlklubLibrary/Interfaces/IMember.cs
+++ SejlklubLibrary/Interfaces/IMember.cs
@@ -9,12 +9,15 @@ namespace SejlklubLibrary.Interfaces
 {
     public interface IMember
     {
+        string MemberImage { get; set; }
         MemberType MemberStatus { get; set; }
         string Phone { get; set;}
         string Email { get; set; }
         int Id {  get; }
         string Address { get; set; }
         string Name { get; set; }
+        bool IsCoach { get; set; }
+        bool IsAdmin { get; set; }
 
         string ToString();
     }
